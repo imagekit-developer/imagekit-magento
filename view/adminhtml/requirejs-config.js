2d833149174e7dcc78cb2ffbfbc5b4891c116354
Abhinav Dhiman <abhinav@imagekit.io>
2024-07-20 20:07:50 +0530

    fix: locked imagekit-media-library-widget to 1.0.4

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,5 +1,5 @@
 var config = {
   paths: {
-    imagekitMediaLibrary: "//unpkg.com/imagekit-media-library-widget/dist/imagekit-media-library-widget.min"
+    imagekitMediaLibrary: "//unpkg.com/imagekit-media-library-widget@1.0.4/dist/imagekit-media-library-widget.min"
   }
 }
