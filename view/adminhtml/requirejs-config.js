f0a96c2a9a3b1ce4f0de4feb0e67a93c8bb66e60
Abhinav Dhiman <abhinav@imagekit.io>
2026-02-19 21:16:21 +0530

    chore: updated imagekit-media-library-widget to 2.4.1

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,5 +1,5 @@
 var config = {
   paths: {
-    imagekitMediaLibrary: "//unpkg.com/imagekit-media-library-widget@1.0.4/dist/imagekit-media-library-widget.min"
+    imagekitMediaLibrary: "//unpkg.com/imagekit-media-library-widget@2.4.1/dist/imagekit-media-library-widget.min"
   }
 }
